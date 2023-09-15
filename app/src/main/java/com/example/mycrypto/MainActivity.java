faf2a4f290b9e177e44b2abff3e7ef31326c2015
quanghuy <huy98746@gmail.com>
2023-09-15 13:17:49 +0700

    first commit

---
+++ app/src/main/java/com/example/mycrypto/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.mycrypto;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+import com.example.mycrypto.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new Fragment());
+
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.Dashboard) {
+                replaceFragment(new DashboardFragment());
+            } else if (item.getItemId() == R.id.Account) {
+                replaceFragment(new AccountFragment());
+            } else if (item.getItemId() == R.id.Setting) {
+                replaceFragment(new SettingFragment());
+            }
+            return true;
+        });
+
+    }
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();}
+}
